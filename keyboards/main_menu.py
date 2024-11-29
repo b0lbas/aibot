5fa2d1916817fbff5c897ea409db654b2e98e71a
b0lbas <sumericforden@gmail.com>
2024-11-29 02:06:27 +0100

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,9 +1,6 @@
-# keyboards/main_menu.py
-
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
-    """Create the main menu keyboard."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(KeyboardButton('Get custom logo'))
     markup.add(KeyboardButton('Random logo'))
