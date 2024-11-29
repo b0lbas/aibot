ec6e9bc7a3bcff029915c44649ebc4b089158e9a
b0lbas <sumericforden@gmail.com>
2024-11-29 02:07:12 +0100

    Update image_handler.py

--- handlers/image_handler.py
+++ handlers/image_handler.py
@@ -1,10 +1,7 @@
-# handlers/image_handler.py
-
 from huggingface_hub import InferenceClient
 from handlers.common import send_image
 from configfile import HF_MODEL, HF_TOKEN
 
-# Initialize the image generation client
 image_client = InferenceClient(HF_MODEL, token=HF_TOKEN)
 
 def generate_custom_image(message, bot):
