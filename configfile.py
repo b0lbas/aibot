17120d238b760c0e72ae162971d7ff6db3445075
Uladzislau Bolbas <vladislav@bolbas.dev>
2024-12-16 10:46:19 +0100

    Delete configfile.py

--- configfile.py
+++
@@ -1,4 +0,0 @@
-TOKEN = '7292077061:AAHcmyDb_-Z58HQUhhZzi1sMdAo0qs9wi-c'
-HF_MODEL = "anthienlong/enhanceaiteam_uncensored"
-HF_INSTRUCTION_MODEL = "Qwen/Qwen2.5-1.5B-Instruct"
-HF_TOKEN = "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"
